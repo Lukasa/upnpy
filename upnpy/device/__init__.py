73b5bb04415ab62a3f220efa3102ce4531008134
Cory Benfield <lukasaoz@gmail.com>
2013-06-29 08:31:56 +0100

    We need the whole API.

--- upnpy/device/__init__.py
+++ upnpy/device/__init__.py
@@ -5,4 +5,4 @@ upnpy.device
 
 This module contains classes that define behaviours of UPnP devices.
 """
-from .device import Device
+from .device import Device, device_from_httpu_response, device_map
