1250be9581d34280384802861b801a11707632d9
Cory Benfield <lukasaoz@gmail.com>
2013-07-07 17:07:07 +0100

    RPC command first pass.

--- upnpy/service/service.py
+++ upnpy/service/service.py
@@ -6,6 +6,8 @@ service.py
 Define a base Service class. This is the class that is used when we don't know
 anything about a given Service.
 """
+import xml.etree.ElementTree as ET
+from ..utils import get_SOAP_RPC_base
 
 
 class Service(object):
@@ -22,6 +24,9 @@ class Service(object):
                       its XML tags.
     """
     def __init__(self, parent, service_root, service_type, namespace):
+        #: The parent device.
+        self.parent = parent
+
         #: The UPnP type of the service, e.g.
         #: "urn:schemas-upnp-org:service:Layer3Forwarding:1".
         self.service_type = service_type
@@ -38,3 +43,59 @@ class Service(object):
 
         #: The URL for subscribing to events.
         self.event_sub_url = service_root.find(namespace + 'eventSubURL').text
+
+    def __send_RPC_command(self,
+                           action_name,
+                           xml_command=None,
+                           soap_args=None):
+        """
+        Prepares and sends a SOAP RPC command. Adds the general structure
+        required for the RPC command, and then sends that command to this
+        device.
+
+        Returns the Reqeusts :class:`Response <requests.Response>` object from
+        the HTTP POST.
+
+        :param action_name: The name (including version) of the action to
+                            perform.
+        :param xml_command: (optional) An ElementTree node representing the
+                            root of the SOAP envelope body. If you don't
+                            provide this, you must provide ``soap_args``.
+        :param soap_args:  (optional) A dictionary of arguments to provide on
+                           the RPC call. This should be used whenever you can
+                           represent the arguments in this way. If this isn't
+                           provided, you must provide ``xml_command``.
+        """
+        if (xml_command is None) and (soap_args is None):
+            raise ValueError("May not provide both xml_command and soap_args")
+
+        # Build the default headers.
+        headers = {'CONTENT-TYPE': 'text/xml; charset="utf-8"',
+                   'SOAPACTION': service + '#' + action_name}
+
+        # Prepare the skeleton of the XML.
+        root, body = get_SOAP_RPC_base()
+
+        # If we got an XML tree, just append it to the body. Otherwise, if we
+        # have a dictionary, generate XML from that.
+        if xml_command is not None:
+            body.append(xml_command)
+        else:
+            # Add the action-specific tag.
+            append_root = ET.SubElement(body,
+                                        'u:' + action_name,
+                                        {'xmlns:u': service})
+
+            # Each element in the dictionary should have a tag.
+            for argname, argval in soap_args:
+                elem = ET.SubElement(append_root, argname)
+                elem.text = str(argval)
+
+        # Prepare the body string.
+        post_body = '<?xml version="1.0"?>'
+        post_body += ET.tostring(root)
+
+        # Now post it.
+        url = self.parent.base_url + self.control_url
+
+        return requests.post(url, headers=headers, body=post_body)
